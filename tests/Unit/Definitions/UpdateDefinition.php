8e2b937bd39aac22c5114f2d71d4add853868dd4
eserna27 <eserna27@gmail.com>
2019-04-01 10:49:21 -0600

    wip

---
+++ tests/Unit/Definitions/UpdateDefinition.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit\Definitions;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UpdateDefinition extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
